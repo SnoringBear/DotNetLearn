21782b1a0946d5cf3dc2ee5294d482d06560e6d7
anye <hnkjxy2015@outlook.com>
2025-02-25 19:03:21 +0800

    ignore file

--- DataConvert/ExplicitTypeConversion.cs
+++ DataConvert/ExplicitTypeConversion.cs
@@ -2,5 +2,19 @@
 [TestFixture]
 public class ExplicitTypeConversion
 {
-    
+    [Test]
+    public void Test01()
+    {
+        int a = 10;
+        byte b = (byte)a;
+        Console.WriteLine("b:{0}",b);
+    }
+
+    [Test]
+    public void Test02()
+    {
+        long a = long.MaxValue;
+        byte b = (byte)a;
+        Console.WriteLine("byte maxValue:{0},b:{1}",byte.MaxValue,b);
+    }
 }
