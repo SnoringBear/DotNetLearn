4c57d05dd1208a4e657ef659cb6f0cce6e954fae
anye <hnkjxy2015@outlook.com>
2025-02-27 18:46:05 +0800

    event

---
+++ Delegate/Button.cs
@@ -0,0 +1,16 @@
+﻿namespace DotNetLearn.Delegate;
+
+public class Button
+{
+    // 定义一个事件
+    public event EventHandler Click;
+
+    // 引发事件的方法
+    public void OnClick()
+    {
+        if (Click != null)
+        {
+            Click(this, EventArgs.Empty);  // 调用事件
+        }
+    }
+}
---
+++ Delegate/ButtonEventTest.cs
@@ -0,0 +1,22 @@
+﻿namespace DotNetLearn.Delegate;
+
+[TestFixture]
+public class ButtonEventTest
+{
+    [Test]
+    public void Test01()
+    {
+        Button button = new Button();
+       
+        // 订阅事件
+        button.Click += Button_Click;
+
+        // 引发事件
+        button.OnClick();  // 输出 "Button clicked!"
+    }
+
+    private static void Button_Click(object sender, EventArgs e)
+    {
+        Console.WriteLine("Button clicked!");
+    }
+}
