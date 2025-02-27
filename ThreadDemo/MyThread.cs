84e89238a68e94ad9a2a747754a6491b69f1a0ff
anye <hnkjxy2015@outlook.com>
2025-02-27 19:01:51 +0800

    thread

---
+++ ThreadDemo/MyThread.cs
@@ -0,0 +1,11 @@
+﻿
+namespace DotNetLearn.ThreadDemo;
+[TestFixture]
+public class MyThread
+{
+    [Test]
+    public void Test01()
+    {
+        Console.WriteLine("Thread.CurrentThread.Name:{0}",Thread.CurrentThread.Name);
+    }
+}
