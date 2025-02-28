245f5b0e4df9bbef28d3a0195423126633a97bd0
anye <hnkjxy2015@outlook.com>
2025-02-28 16:47:40 +0800

    queue

---
+++ Collection/GenericTest.cs
@@ -0,0 +1,39 @@
+﻿namespace DotNetLearn.Collection;
+[TestFixture]
+public class GenericTest
+{
+    // 泛型（Generic） 允许您延迟编写类或方法中的编程元素的数据类型的规范，直到实际在程序中使用它的时候。换句话说，
+    // 泛型允许您编写一个可以与任何数据类型一起工作的类或方法
+    
+    [Test]
+    public void Test01()
+    {
+        MyGenericArray<int> intArray = new MyGenericArray<int>(5);
+        for (int c = 0; c < 5; c++)
+        {
+            intArray.setItem(c, c*5);
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("position:{0},value:{1}",i,intArray.getItem(i));
+        }
+    }
+    
+    public class MyGenericArray<T>
+    {
+        private T[] array;
+        public MyGenericArray(int size)
+        {
+            array = new T[size + 1];
+        }
+        public T getItem(int index)
+        {
+            return array[index];
+        }
+        public void setItem(int index, T value)
+        {
+            array[index] = value;
+        }
+    }
+}
--- Collection/QueueTest.cs
+++ Collection/QueueTest.cs
@@ -1,10 +1,35 @@
-﻿namespace DotNetLearn.Collection;
+﻿using System.Collections;
+
+namespace DotNetLearn.Collection;
 [TestFixture]
 public class QueueTest
 {
+    // 在 C# 中，Queue 是一个先进先出（FIFO, First In First Out）数据结构
+    // Queue 适用于需要按照入队顺序处理数据的场景。
+    // Queue 本身不是线程安全的，但可以使用 ConcurrentQueue<T> 实现线程安全。
+    
     [Test]
     public void Test01()
     {
+        Queue<String> queue = new Queue<String>();
+
+        // 添加元素
+        queue.Enqueue("First");
+        queue.Enqueue("Second");
+        queue.Enqueue("Third");
         
+        Console.WriteLine("queue first:{0}",queue.Peek());
+        
+        foreach (string s in queue)
+        {
+            Console.WriteLine("s:{0}",s);
+        }
+        
+        // 检查包含
+        Console.WriteLine($"Contains 'Second': {queue.Contains("Second")}"); // 输出：True
+
+        // 转数组
+        object[] array = queue.ToArray();
+        Console.WriteLine($"Array Length: {array.Length}"); // 输出：2
     }
 }
