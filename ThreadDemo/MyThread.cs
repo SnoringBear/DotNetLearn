22112e3032d72186299e663f6f8ab60228555935
anye <hnkjxy2015@outlook.com>
2025-02-28 10:08:01 +0800

    thread

--- ThreadDemo/MyThread.cs
+++ ThreadDemo/MyThread.cs
@@ -8,4 +8,17 @@ public class MyThread
     {
         Console.WriteLine("Thread.CurrentThread.Name:{0}",Thread.CurrentThread.Name);
     }
+
+    [Test]
+    public void Test02()
+    {
+        void Start() => Console.WriteLine("Thread.CurrentThread.Name:{0}", Thread.CurrentThread.Name);
+        var thread = new Thread(Start)
+        {
+            Name = "Test02"
+        };
+        thread.Start();
+        Thread.Sleep(1000);
+        Console.WriteLine("执行结束");
+    }
 }
