396b22edeae8307ece73d8b6befe7713ba55cb10
anye <hnkjxy2015@outlook.com>
2025-03-07 18:02:55 +0800

    AssemblyTest

---
+++ DataType/AssemblyTest.cs
@@ -0,0 +1,15 @@
+﻿namespace DotNetLearn.DataType;
+[TestFixture]
+public class AssemblyTest
+{
+    [Test]
+    public void Test01()
+    {
+        Console.WriteLine(typeof(AssemblyTest).Assembly);
+        Console.WriteLine("foreach GetTypes()-----");
+        foreach (var type in typeof(AssemblyTest).Assembly.GetTypes())
+        {
+            Console.WriteLine(type);
+        }
+    }
+}
